9013745ec23df7dcd7946d5c8c0c7743873eb4b2
Deven Ramani <Deven.Ramani@RapidCircle.com>
2019-03-21 01:02:05 +0530

    Updated title

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header">Git-Search
           <img src={logo} className="App-logo" alt="logo" />
 
           <SearchBox
